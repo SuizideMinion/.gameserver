217fe2deb6e7c403fd71d08b9c535331e700f413
Daniel Helmbrecht <dieerben@gmail.com>
2022-12-03 22:28:01 +0100

    .

--- resources/views/layout/ingame.blade.php
+++ resources/views/layout/ingame.blade.php
@@ -117,13 +117,72 @@
 <script>
     $.ajax({
         url: "/api/crown",
-        // type: 'GET',
-        // dataType: 'json', // added data type
         success: function(res) {
             console.log(res);
-            // alert(res);
         }
     });
+</script>
+<script>
+</script>
+<script type="text/javascript">
+    window.onload = function() {
+        window.notify = {
+            list: [],
+            id: 0,
+            log: function(msg) {
+                var console = document.getElementById('console');
+                console.innerHTML += ("\n"+msg);
+                console.scrollTop = console.scrollHeight;
+            },
+            compatible: function() {
+                if (typeof Notification === 'undefined') {
+                    notify.log("Notifications are not available for your browser.");
+                    return false;
+                }
+                return true;
+            },
+            authorize: function() {
+                if (notify.compatible()) {
+                    Notification.requestPermission(function(permission) {
+                        notify.log("Permission to display: "+permission);
+                    });
+                }
+            },
+            showDelayed: function(seconds) {
+                notify.log("A notification will be triggered in "+seconds+" seconds. Try minimising the browser now.");
+                setTimeout(notify.show('text'), (seconds*1000));
+            },
+            show: function(text) {
+
+                if (typeof Notification === 'undefined') { notify.log("Notifications are not available for your browser."); return; }
+                if (notify.compatible()) {
+                    notify.id++;
+                    var id = notify.id;
+                    notify.list[id] = new Notification("Notification #"+id, {
+                        body: text,
+                        tag: id,
+                        icon: "images/Sexy_Ben.jpeg",
+                        lang: "",
+                        dir: "auto",
+                    });
+                    notify.log("Notification #"+id+" queued for display");
+                    notify.list[id].onclick = function() { notify.logEvent(id, "clicked"); };
+                    notify.list[id].onshow  = function() { notify.logEvent(id, "showed");  };
+                    notify.list[id].onerror = function() { notify.logEvent(id, "errored"); };
+                    notify.list[id].onclose = function() { notify.logEvent(id, "closed");  };
+
+                    console.log("Created a new notification ...");
+                    console.log(notify.list[id]);
+                }
+            },
+            logEvent: function(id, event) {
+                notify.log("Notification #"+id+" "+event);
+            }
+        };
+        notify.showDelayed({{ ( (\App\Models\UserBuildings::where('user_id', auth()->user()->id)->where('value', 1)->first()->time ?? 99999999999) - time() ) }})
+        notify.showDelayed({{ ( (\App\Models\UserResearchs::where('user_id', auth()->user()->id)->where('value', 1)->first()->time ?? 99999999999) - time() ) }})
+
+    };
 </script>
 </body>
 
