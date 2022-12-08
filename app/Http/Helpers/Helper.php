79b32e3a7018d941a73bec0799fc94d066b6a94d
Daniel Helmbrecht <dieerben@gmail.com>
2022-12-08 20:23:16 +0100

    .

--- app/Http/Helpers/Helper.php
+++ app/Http/Helpers/Helper.php
@@ -30,6 +30,7 @@ function ressCalc($id = 0)
             'ress2' => $ress2,
             'ress3' => $ress3,
             'ress4' => $ress4,
+            'ress5' => 0,
         ])
     ]);
     // M 2:1 | D 4:1 | I 6:1 | E 8:1
@@ -56,6 +57,11 @@ function uData($key)
 function JSONuData($key)
 {
     return json_decode(session('uData')[$key]->value ?? false);
+}
+
+function ServerData($key)
+{
+    return session('ServerData')[$key]->value ?? false;
 }
 
 function uRess()
@@ -87,6 +93,82 @@ function timeconversion($sekunden): string
 function getImage($name, $path = null, $race = null): string
 {
     return '/assets/img' . ($path != null ? '/' . $path : '') . '/' . $race . $name;
+}
+
+function FormatTime($timestamp)
+{
+    // Get time difference and setup arrays
+    $difference = time() - $timestamp;
+    $periods = array("second", "minute", "hour", "day", "week", "month", "years");
+    $lengths = array("60","60","24","7","4.35","12");
+
+    // Past or present
+    if ($difference >= 0)
+    {
+        $ending = "ago";
+    }
+    else
+    {
+        $difference = -$difference;
+        $ending = "to go";
+    }
+
+    // Figure out difference by looping while less than array length
+    // and difference is larger than lengths.
+    $arr_len = count($lengths);
+    for($j = 0; $j < $arr_len && $difference >= $lengths[$j]; $j++)
+    {
+        $difference /= $lengths[$j];
+    }
+
+    // Round up
+    $difference = round($difference);
+
+    // Make plural if needed
+    if($difference != 1)
+    {
+        $periods[$j].= "s";
+    }
+
+    // Default format
+    $text = "$difference $periods[$j] $ending";
+
+    // over 24 hours
+    if($j > 2)
+    {
+        // future date over a day formate with year
+        if($ending == "to go")
+        {
+            if($j == 3 && $difference == 1)
+            {
+                $text = "Tomorrow at ". date("g:i a", $timestamp);
+            }
+            else
+            {
+                $text = date("F j, Y \a\\t g:i a", $timestamp);
+            }
+            return $text;
+        }
+
+        if($j == 3 && $difference == 1) // Yesterday
+        {
+            $text = "Yesterday at ". date("g:i a", $timestamp);
+        }
+        else if($j == 3) // Less than a week display -- Monday at 5:28pm
+        {
+            $text = date("l \a\\t g:i a", $timestamp);
+        }
+        else if($j < 6 && !($j == 5 && $difference == 12)) // Less than a year display -- June 25 at 5:23am
+        {
+            $text = date("F j \a\\t g:i a", $timestamp);
+        }
+        else // if over a year or the same month one year ago -- June 30, 2010 at 5:34pm
+        {
+            $text = date("F j, Y \a\\t g:i a", $timestamp);
+        }
+    }
+
+    return $text;
 }
 
 function number_shorten($number, $precision = 3, $divisors = null): string
