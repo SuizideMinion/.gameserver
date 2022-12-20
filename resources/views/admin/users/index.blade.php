20e6936cbf0b7186d77d0dc9081ec8c41a411282
Daniel Helmbrecht <dieerben@gmail.com>
2022-12-20 22:16:35 +0100

    .

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,42 +3,31 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12">
-
             <div class="card">
                 <div class="card-body">
-
-                    <!-- Table with stripped rows -->
-                    <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
-                        <div class="dataTable-top">
-                            <div class="dataTable-search"><input type="text" id="dataSearch" onkeyup="myFunction()" placeholder="Suche ..."
-                                                                 title="Type in a name"></div>
-                        </div>
-                        <div class="dataTable-container">
-                            <table class="table dataTable" id="dataTable">
-                                <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Registriert</th>
-                                    <th scope="col">Letzter Update</th>
-                                    <th scope="col">Letzter Klick</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($Users AS $User)
-                                    <tr>
-                                        <th scope="row">{{ $User->id }}</th>
-                                        <td>{{ $User->name }}</td>
-                                        <td>{{ $User->created_at->format('d.m.Y H:i:s') }}</td>
-                                        <td>{{ $User->updated_at->format('d.m.Y H:i:s') }}</td>
-                                        <td>{{ date('d.m.Y H:i:s', ($User->getData->pluck('value', 'key')['lastclick'] ?? 0) ) }}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-
+                    <h5 class="card-title">Übersetzungen</h5>
+                    <table class="table datatable" id="dataTable">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Registriert</th>
+                            <th scope="col">Letzter Update</th>
+                            <th scope="col">Letzter Klick</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($Users AS $User)
+                            <tr>
+                                <th scope="row">{{ $User->id }}</th>
+                                <td>{{ $User->name }}</td>
+                                <td>{{ $User->created_at->format('d.m.Y H:i:s') }}</td>
+                                <td>{{ $User->updated_at->format('d.m.Y H:i:s') }}</td>
+                                <td>{{ date('d.m.Y H:i:s', ($User->getData->pluck('value', 'key')['lastclick'] ?? 0) ) }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
